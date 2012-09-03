8424440f08b4c2d27e2619dc490d5ed0343bc173
root <root@ubuntu.(none)>
2012-09-02 19:04:46 -0600

    added tornado server added basic init.d script (needs much improvement) added *.pyc to gitignore (not sure why this wasn't here before)

---
+++ cyclone.py
@@ -0,0 +1,17 @@
+import daemon
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from icontrim import app
+
+port = 5000
+
+log = open('/var/log/tornado/tornado.' + str(port) + '.log', 'a+')
+log_stream_descriptor = log.fileno()
+ctx = daemon.DaemonContext(files_preserve=[log_stream_descriptor], gid=0, uid=0, stdout=log, stderr=log,  working_directory='.')
+ctx.open()
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(port)
+
+IOLoop.instance().start()
